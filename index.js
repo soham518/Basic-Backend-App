3b303d77cff91c034f0ae96e61bb6774eabaa8b6
soham518 <167617833+soham518@users.noreply.github.com>
2025-07-06 22:18:13 +0530

    added json data get request

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const jsonData = {
+  "name": "John Doe",
+  "age": 30,
+  "city": "New York"
+}
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -17,6 +22,9 @@ app.get("/login", (req, res) => {
 
 app.get("/portfolio",(req,res)=> {
     res.send("<h2>Soham's Portfolio</h2>")
+})
+app.get("/jsondata",(req,res)=> {
+    res.json(jsonData)
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`);
